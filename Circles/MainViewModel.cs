08c7eeaeb955e7aeb543e4a1868a97100fb293c9
Riz <111>
2024-01-28 18:52:18 +0500

    Добавьте файлы проекта.

---
+++ Circles/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Circles.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Circles;
+
+internal class MainViewModel : INotifyPropertyChanged
+{
+    private ObservableCollection<FigureModel> figures;
+
+    public MainViewModel()
+    {
+        Figures = new ObservableCollection<FigureModel>()
+        {
+            new FigureModel()
+            {
+                X = 50,
+                Y = 50,
+                Radius = 30
+            },
+            new FigureModel()
+            {
+                X = 100,
+                Y = 50,
+                Radius = 40
+            },
+            new FigureModel()
+            {
+                X = 150,
+                Y = 50,
+                Radius = 20
+            },
+        };
+    }
+
+    public ObservableCollection<FigureModel> Figures
+    {
+        get => figures;
+        set
+        {
+            figures = value;
+            //OnPropertyChanged();
+        }
+    }
+
+
+    #region MVVM 
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(prop));
+    }
+    #endregion
+}
---
+++ Circles/Models/FigureModel.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace Circles.Models;
+
+internal class FigureModel
+{
+    private int _x;
+    private int _y;
+    private int _radius;
+
+    private Brush _fillColor;
+    private Brush _borderColor;
+
+    public int X
+    {
+        get => _x;
+        set
+        {
+            _x = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int Y
+    {
+        get => _y;
+        set
+        {
+            _y = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int Radius
+    {
+        get => _radius;
+        set
+        {
+            _radius = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Brush FillColor
+    {
+        get => _fillColor;
+        set
+        {
+            _fillColor = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Brush BorderColor
+    {
+        get => _borderColor;
+        set
+        {
+            _borderColor = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(prop));
+    }
+}
